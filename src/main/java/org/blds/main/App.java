278b7a106fa22c60bb2b8c66ba20373870a64989
andrzejbaldys1990@gmail.com <user@dev-java.blds.org>
2019-01-20 20:25:30 +0100

    init

---
+++ src/main/java/org/blds/main/App.java
@@ -0,0 +1,18 @@
+package org.blds.main;
+
+public class App 
+{
+    static int add(int a, int b){
+        return a + b;
+    }
+
+//    static int diff(int a, int b){
+//        return a - b;
+//    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( add(1, 1 ) );
+//        System.out.println( diff(1, 1 ) );
+    }
+}
---
+++ src/test/java/org/blds/main/AppTest.java
@@ -0,0 +1,22 @@
+package org.blds.main;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertEquals( App.add(1, 1), 2 );
+//        assertEquals( App.diff(1, 1), 0);
+    }
+}
